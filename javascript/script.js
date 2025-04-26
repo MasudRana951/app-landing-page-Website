f4ac3325c5cd6c21eea604ecdcbe219a6c0d988e
MD: Masud Rana <118680879+MasudRana951@users.noreply.github.com>
2025-04-27 02:23:38 +0600

    website for app landing page
    Finished the website, and 90% is already responsive just need to do some touching work

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.screenshot-section-items').slick({
+      autoplay:true,
+      centerMode: false,
+      infinite: true,
+      arrows: false,
+      initialSlide: 3,
+      slidesToShow: 3,
+      pauseOnHover: false,
+      autoplaySpeed: 2000,
+      dots: true,
+    });
+  });
